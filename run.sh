65a8ee7e9ed5301bfa2e62d26bbe4d13cc950c96
Roman Haritonov <rharitonov@asdco.ru>
2019-07-24 15:10:40 +0300

    Update launch command for DB usage [2]

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 NAME=ppbot
-DB_LOCATION=/tg_pp_bot.db
+DB_LOCATION=/db/
 
 docker build -t ${NAME} .
 docker rm -f ${NAME} || true
-docker run --name ${NAME} -d --restart=unless-stopped -e PP_BOT_TOKEN=${PP_BOT_TOKEN} -e PP_BOT_DB_PATH=${DB_LOCATION} -v ~/.tg_pp_bot.db:${DB_LOCATION} ${NAME}
+docker run --name ${NAME} -d --restart=unless-stopped -e PP_BOT_TOKEN=${PP_BOT_TOKEN} -e PP_BOT_DB_PATH=${DB_LOCATION}/tb_pp_bot.db -v ~/.ppbot/:${DB_LOCATION} ${NAME}
 docker logs -f ${NAME}
