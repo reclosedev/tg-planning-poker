a788773505de72c6a9401cee37ed2c6dee2c9c01
Roman Haritonov <rharitonov@asdco.ru>
2019-07-25 13:07:19 +0300

    Typo in db name

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 NAME=ppbot
 DB_LOCATION=/db/
+DB_NAME=tg_pp_bot.db
 
 docker build -t ${NAME} .
 docker rm -f ${NAME} || true
-docker run --name ${NAME} -d --restart=unless-stopped -e PP_BOT_TOKEN=${PP_BOT_TOKEN} -e PP_BOT_DB_PATH=${DB_LOCATION}/tb_pp_bot.db -v ~/.ppbot/:${DB_LOCATION} ${NAME}
+docker run --name ${NAME} -d --restart=unless-stopped -e PP_BOT_TOKEN=${PP_BOT_TOKEN} -e PP_BOT_DB_PATH=${DB_LOCATION}/${DB_NAME} -v ~/.ppbot/:${DB_LOCATION} ${NAME}
 docker logs -f ${NAME}
